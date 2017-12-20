1a77b244b3b83c012cad0202161c7cee23dd54e0
asancen <asancenp@gmail.com>
2017-12-20 13:13:35 -0600

    Primera versión

---
+++ ETBSA.Lubricantes/ETBSA.Presentation.Front.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETBSA.Presentation.Front.Web.Startup))]
+namespace ETBSA.Presentation.Front.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ ETBSA.Lubricantes/ETBSA.Presentation.Front.Web/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ETBSA.Presentation.Front.Web.MvcApplication" Language="C#" %>
