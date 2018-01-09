893ded7549199a992365cccff4ee436db4e91d34
asancen <asancenp@gmail.com>
2018-01-09 13:07:08 -0600

    Diseño aprobado por el cliente 8 ene 2017

---
+++ ETBSA.Lubricantes/ETBSA.Presentation.Front.Web2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETBSA.Presentation.Front.Web2.Startup))]
+namespace ETBSA.Presentation.Front.Web2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
